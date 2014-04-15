23cdf3b51c07255e4bd1d4240c41f6e4a6fef95f
Mindaugas <Mindaugas@172.30.9.70>
2014-04-15 14:32:31 +0100

    Complited AddCustomerGUI.

--- src/views/AddCustomerGUI.java
+++ src/views/AddCustomerGUI.java
@@ -6,10 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AddCustomerGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField tfName;
+	private JTextField tfEmail;
+	private JTextField tfPhone;
+	private JTextField tfAddress;
 
 	/**
 	 * Launch the application.
@@ -32,11 +42,74 @@ public class AddCustomerGUI extends JFrame {
 	 */
 	public AddCustomerGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 380, 265);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblName = new JLabel("Name");
+		lblName.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblName.setBounds(30, 6, 98, 35);
+		contentPane.add(lblName);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblEmail.setBounds(30, 48, 98, 35);
+		contentPane.add(lblEmail);
+		
+		JLabel lblPhone = new JLabel("Phone");
+		lblPhone.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblPhone.setBounds(30, 95, 98, 35);
+		contentPane.add(lblPhone);
+		
+		JLabel lblAddress = new JLabel("Address");
+		lblAddress.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblAddress.setBounds(30, 142, 98, 35);
+		contentPane.add(lblAddress);
+		
+		tfName = new JTextField();
+		tfName.setBounds(140, 9, 200, 35);
+		contentPane.add(tfName);
+		tfName.setColumns(10);
+		
+		tfEmail = new JTextField();
+		tfEmail.setBounds(140, 53, 200, 35);
+		contentPane.add(tfEmail);
+		tfEmail.setColumns(10);
+		
+		tfPhone = new JTextField();
+		tfPhone.setBounds(140, 100, 200, 35);
+		contentPane.add(tfPhone);
+		tfPhone.setColumns(10);
+		
+		tfAddress = new JTextField();
+		tfAddress.setBounds(140, 147, 200, 35);
+		contentPane.add(tfAddress);
+		tfAddress.setColumns(10);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				dispose();
+				
+			}
+		});
+		btnCancel.setBounds(140, 194, 100, 30);
+		contentPane.add(btnCancel);
+		
+		JButton btnOk = new JButton("Ok");
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				// TODO Pass user's input to the confirmation GUI
+				
+				dispose();
+				
+			}
+		});
+		btnOk.setBounds(240, 194, 100, 30);
+		contentPane.add(btnOk);
 	}
-
 }
