cad5c40424b9bfa66256c1198c7f2b70a65c3339
JSterling8 <u1276062@unimail.hud.ac.uk>
2014-04-15 11:31:01 +0100

    Resized MainGUI and removed ActionListeners for now.

--- src/views/MainGUI.java
+++ src/views/MainGUI.java
@@ -10,8 +10,6 @@ import javax.swing.UIManager;
 
 @SuppressWarnings("serial")
 public class MainGUI extends javax.swing.JFrame {
-	private RemindersGUI remindersGUI;
-	private StockGUI stockGUI;
 	private JFrame frame;
 
 	/**
@@ -53,48 +51,24 @@ public class MainGUI extends javax.swing.JFrame {
 		frame.getContentPane().setLayout(null);
 		
 		JButton btnStock = new JButton("Stock");
-		btnStock.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				stockGUI.setVisible(true);
-				remindersGUI.setVisible(false);
-			}
-		});
-		btnStock.setBounds(10, 11, 67, 21);
+		btnStock.setBounds(5, 11, 130, 34);
 		frame.getContentPane().add(btnStock);
 		
 		JButton btnSuppliers = new JButton("Suppliers");
-		btnSuppliers.setBounds(84, 11, 75, 21);
+		btnSuppliers.setBounds(140, 11, 130, 34);
 		frame.getContentPane().add(btnSuppliers);
 		
 		JButton btnCustomers = new JButton("Customers");
-		btnCustomers.setBounds(159, 11, 100, 21);
+		btnCustomers.setBounds(275, 11, 130, 34);
 		frame.getContentPane().add(btnCustomers);
 		
 		JButton btnTransactions = new JButton("Transactions");
-		btnTransactions.setBounds(266, 11, 124, 21);
+		btnTransactions.setBounds(410, 11, 130, 34);
 		frame.getContentPane().add(btnTransactions);
 		
-		JButton btnProfitlossOverview = new JButton("Profit/Loss Overview");
-		btnProfitlossOverview.setBounds(398, 11, 173, 21);
-		frame.getContentPane().add(btnProfitlossOverview);
-		
 		JButton btnReminders = new JButton("Reminders");
-		btnReminders.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				remindersGUI.setVisible(true);
-				stockGUI.setVisible(false);
-			}
-		});
-		btnReminders.setBounds(581, 10, 89, 23);
+		btnReminders.setBounds(545, 10, 130, 35);
 		frame.getContentPane().add(btnReminders);
-		
-		remindersGUI = new RemindersGUI();
-		remindersGUI.setBounds(10, 32, 665, 231);
-		frame.getContentPane().add(remindersGUI);
-		
-		stockGUI = new StockGUI();
-		stockGUI.setBounds(10, 32, 665, 231);
-		frame.getContentPane().add(stockGUI);
 		
 		
 
