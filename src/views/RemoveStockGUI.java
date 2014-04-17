1c0ff8ef2c96ebf4e7fbfd7883cdd95822a8d8be
JSterling8 <u1276062@unimail.hud.ac.uk>
2014-04-17 10:18:33 +0100

    Changed font in RemoveStockGUI to Tahoma.

--- src/views/RemoveStockGUI.java
+++ src/views/RemoveStockGUI.java
@@ -1,23 +1,18 @@
 package views;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.text.MaskFormatter;
-import javax.swing.JFormattedTextField;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-
 import java.awt.Font;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.text.NumberFormat;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class RemoveStockGUI extends JFrame {
 
@@ -47,7 +42,7 @@ public class RemoveStockGUI extends JFrame {
 	public RemoveStockGUI() {
 		
 		setTitle("Remove Stock");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 400, 210);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -87,7 +82,7 @@ public class RemoveStockGUI extends JFrame {
 		contentPane.add(btnCancel);
 		
 		tfUnits = new JTextField();
-		tfUnits.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		tfUnits.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		tfUnits.setBounds(138, 36, 213, 35);
 		contentPane.add(tfUnits);
 		tfUnits.setColumns(10);
@@ -103,7 +98,7 @@ public class RemoveStockGUI extends JFrame {
         });
 		
 		tfReason = new JTextField();
-		tfReason.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		tfReason.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		tfReason.setBounds(138, 83, 213, 35);
 		contentPane.add(tfReason);
 		tfReason.setColumns(10);
