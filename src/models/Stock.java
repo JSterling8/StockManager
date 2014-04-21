0eca10a4f571289a624f97ad8ed5aecb63f8df46
Mindaugas <Mindaugas@172.30.9.70>
2014-04-21 10:38:05 +0100

    Completed Stock model.

--- src/models/Stock.java
+++ src/models/Stock.java
@@ -1,5 +1,63 @@
 package models;
 
 public class Stock {
+	
+	private String productName;
+	private Supplier supplier;
+	private double quantity;
+	private double price;
+	
+	public Stock(String productName, Supplier supplier, double quantity, double price) {
+		
+		this.productName = productName;
+		this.supplier = supplier;
+		this.quantity = quantity;
+		this.price = price;
+		
+	}
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the productName
+	 */
+	public String getProductName() {
+		return productName;
+	}
+
+	/**
+	 * @return the supplier
+	 */
+	public Supplier getSupplier() {
+		return supplier;
+	}
+
+	/**
+	 * @return the quantity
+	 */
+	public double getQuantity() {
+		return quantity;
+	}
+	
+	public void addProduct(double amount) {
+		this.quantity = quantity + amount;
+	}
+	
+	public void removeProduct(double amount) {
+		this.quantity = quantity - amount;
+	}
+	
 
 }
