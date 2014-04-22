88bdf3663fb134143683250a0c11481c46386a55
JSterling8 <u1276062@unimail.hud.ac.uk>
2014-04-22 11:29:43 +0100

    productList now initializes with whatever is in the stockList.

--- src/controllers/ProductController.java
+++ src/controllers/ProductController.java
@@ -7,11 +7,9 @@ public class ProductController {
 public static ArrayList<String> productList = new ArrayList<String>();
 	
 	public ProductController(){
-		productList.add("Product 1");
-		productList.add("Product 2");
-		productList.add("Product 3");
-		productList.add("Product 4");
-		productList.add("Product 5");
+		for(int i = 0; i < StockController.stockList.size(); i++){
+			productList.add(StockController.stockList.get(i).getProductName());
+		}
 
 	}
 	
