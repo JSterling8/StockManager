1f0720c3eee4dfc73693fab946058948bd608d38
JSterling8 <u1276062@unimail.hud.ac.uk>
2014-04-22 12:25:32 +0100

    supplierList now initalized with stockList's suppliers.

--- src/controllers/SupplierController.java
+++ src/controllers/SupplierController.java
@@ -6,16 +6,14 @@ import models.Supplier;
 
 public class SupplierController {
 
-public static ArrayList<Supplier> supplierList;
-	
+	public static ArrayList<Supplier> supplierList;	
+
 	public SupplierController(){
 		supplierList = new ArrayList<Supplier>();
-		
-		supplierList.add(new Supplier("Supplier 0", 0));
-		supplierList.add(new Supplier("Supplier 1", 1));
-		supplierList.add(new Supplier("Supplier 2", 2));
-		supplierList.add(new Supplier("Supplier 3", 3));
-		supplierList.add(new Supplier("Supplier 4", 4));
+
+		for(int i = 0; i < StockController.stockList.size(); i++){
+			supplierList.add(StockController.stockList.get(i).getSupplier());
+		}
+
 	}
-	
 }
