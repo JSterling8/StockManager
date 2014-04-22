c8e36d5318daead221019b392815c87813adc256
JSterling8 <u1276062@unimail.hud.ac.uk>
2014-04-22 11:36:03 +0100

    Added static addProduct method to ProductController.

--- src/controllers/ProductController.java
+++ src/controllers/ProductController.java
@@ -4,13 +4,22 @@ package controllers;
 import java.util.ArrayList;
 
 public class ProductController {
-public static ArrayList<String> productList = new ArrayList<String>();
-	
+	public static ArrayList<String> productList = new ArrayList<String>();
+
 	public ProductController(){
 		for(int i = 0; i < StockController.stockList.size(); i++){
 			productList.add(StockController.stockList.get(i).getProductName());
 		}
-
 	}
-	
+
+	public static void addProduct(String productName){
+		for(int i = 0; i < productList.size(); i++){
+			if (!productList.get(i).equalsIgnoreCase(productName)){
+				productList.add(productName);
+			}
+		}
+	}
+
+
+
 }
