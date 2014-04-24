c0d64b556df20360cb0388340c5d6f82549fa57d
Mindaugas Raudavicius <mindaugas.raudavicius@gmail.com>
2014-04-24 15:17:38 +0100

    Date picker has been added to SellStockGUI

--- src/views/SellStockGUI.java
+++ src/views/SellStockGUI.java
@@ -36,6 +36,9 @@ import controllers.StockController;
 
 public class SellStockGUI extends JFrame {
 
+	private JComboBox cbDay;
+	private JComboBox cbMonth;
+	private JComboBox cbYear;
 	private static final long serialVersionUID = -1802942040864294457L;
 	private JPanel contentPane;
 	private JTable table;
@@ -86,7 +89,7 @@ public class SellStockGUI extends JFrame {
 
 		setTitle("Sell Stock");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 722, 621);
+		setBounds(100, 100, 722, 696);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -323,25 +326,25 @@ public class SellStockGUI extends JFrame {
 
 		JLabel lblTotalAmount = new JLabel("Total Amount");
 		lblTotalAmount.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblTotalAmount.setBounds(313, 455, 223, 35);
+		lblTotalAmount.setBounds(313, 527, 223, 35);
 		contentPane.add(lblTotalAmount);
 
 		JLabel lblProfitloss = new JLabel("Profit/Loss");
 		lblProfitloss.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblProfitloss.setBounds(313, 501, 223, 35);
+		lblProfitloss.setBounds(313, 573, 223, 35);
 		contentPane.add(lblProfitloss);
 
 		tfTotalAmount = new JTextField();
 		tfTotalAmount.setEditable(false);
 		tfTotalAmount.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		tfTotalAmount.setBounds(548, 455, 157, 35);
+		tfTotalAmount.setBounds(548, 527, 157, 35);
 		contentPane.add(tfTotalAmount);
 		tfTotalAmount.setColumns(10);
 
 		tfProfitLoss = new JTextField();
 		tfProfitLoss.setEditable(false);
 		tfProfitLoss.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		tfProfitLoss.setBounds(548, 501, 157, 35);
+		tfProfitLoss.setBounds(548, 573, 157, 35);
 		contentPane.add(tfProfitLoss);
 		tfProfitLoss.setColumns(10);
 
@@ -353,7 +356,7 @@ public class SellStockGUI extends JFrame {
 
 			}
 		});
-		btnCancel.setBounds(605, 547, 100, 35);
+		btnCancel.setBounds(605, 619, 100, 35);
 		contentPane.add(btnCancel);
 
 		JButton btnSubmit = new JButton("Submit");
@@ -370,23 +373,58 @@ public class SellStockGUI extends JFrame {
 				}
 			}
 		});
-		btnSubmit.setBounds(493, 547, 100, 35);
+		btnSubmit.setBounds(493, 619, 100, 35);
 		contentPane.add(btnSubmit);
 
 		JLabel lblAttachInvoice = new JLabel("Attach Invoice");
 		lblAttachInvoice.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblAttachInvoice.setBounds(313, 409, 177, 35);
+		lblAttachInvoice.setBounds(313, 481, 177, 35);
 		contentPane.add(lblAttachInvoice);
 
 		JButton btnBrowse = new JButton("Browse");
 		btnBrowse.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		btnBrowse.setBounds(548, 409, 157, 35);
+		btnBrowse.setBounds(548, 481, 157, 35);
 		contentPane.add(btnBrowse);
 
 		JLabel lblQuantityOwned = new JLabel("Available");
 		lblQuantityOwned.setFont(new Font("Tahoma", Font.BOLD, 18));
 		lblQuantityOwned.setBounds(192, 106, 111, 35);
 		contentPane.add(lblQuantityOwned);
+		
+		JLabel label = new JLabel("Due Date");
+		label.setFont(new Font("Tahoma", Font.BOLD, 18));
+		label.setBounds(185, 431, 100, 35);
+		contentPane.add(label);
+		
+		JLabel lblDay = new JLabel("Day:");
+		lblDay.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblDay.setBounds(291, 431, 50, 35);
+		contentPane.add(lblDay);
+		
+		cbDay = new JComboBox();
+		cbDay.setBounds(337, 434, 73, 35);
+		cbDay.setModel(new DefaultComboBoxModel(new Integer[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31}));
+		contentPane.add(cbDay);
+		
+		JLabel lblMonth = new JLabel("Month:");
+		lblMonth.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblMonth.setBounds(416, 431, 63, 35);
+		contentPane.add(lblMonth);
+		
+		cbMonth = new JComboBox();
+		cbMonth.setBounds(475, 434, 73, 35);
+		cbMonth.setModel(new DefaultComboBoxModel(new Integer[] {1,2,3,4,5,6,7,8,9,10,11,12}));
+		contentPane.add(cbMonth);
+		
+		JLabel lblYear = new JLabel("Year:");
+		lblYear.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblYear.setBounds(554, 431, 50, 35);
+		contentPane.add(lblYear);
+		
+		cbYear = new JComboBox();
+		cbYear.setBounds(616, 431, 89, 35);
+		cbYear.setModel(new DefaultComboBoxModel(new Integer[] {2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026,2027, 2028, 2029, 2030}));
+		contentPane.add(cbYear);
 
 		setVisible(true);
 	}
@@ -501,5 +539,17 @@ public class SellStockGUI extends JFrame {
 
 	public ArrayList<Long> getIdList() {
 		return idList;
+	}
+	
+	public int getDay() {	
+		return cbDay.getSelectedIndex();
+	}
+	
+	public int getMonth() {
+		return cbMonth.getSelectedIndex();
+	}
+	
+	public int getYear() {
+		return cbYear.getSelectedIndex();
 	}
 }
