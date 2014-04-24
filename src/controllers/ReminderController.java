f1c3ba8d5da12d8650efe67ca764c5aa890bf1d8
JSterling8 <u1276062@unimail.hud.ac.uk>
2014-04-24 19:41:14 +0100

    Added some more comments to the ReminderController class.

--- src/controllers/ReminderController.java
+++ src/controllers/ReminderController.java
@@ -15,13 +15,20 @@ import models.Reminder;
  *
  */
 public class ReminderController {
-
+	// The master reminderList for the application.
 	public static ArrayList<Reminder> reminderList;
 
+	/**
+	 * This class contains the master reminderList for the application.
+	 * This class is initialized in the MainGUI class.
+	 */
 	public ReminderController() {
 		reminderList = new ArrayList<Reminder>(); 
-		// (long id, boolean buy, boolean sell, double amountToPay, Date dueDate, Company company) {
+
+		// This boolean is used to alternate between customer/supplier when adding to the list.
 		boolean even = true;
+		
+		// Initialize the supplierList with 2 suppliers and 2 customers.
 		for (int i = 0; i < SupplierController.supplierList.size() && i < CustomerController.customerList.size(); i++){
 			Reminder reminder;
 			if(even){
@@ -36,6 +43,15 @@ public class ReminderController {
 		}
 	}
 
+	/**
+	 * Add a reminder to the reminderList
+	 * 
+	 * @param buy True if it is a buy reminder.  False if it is a sell reminder.
+	 * @param sell True if it is a sell reminder.  False if it is a buy reminder.
+	 * @param amountToPay The amount outstanding.
+	 * @param dueDate The date that the payment is due.
+	 * @param company The customer or supplier that is in debt or in credit, respectively.
+	 */
 	public static void addReminder(boolean buy, boolean sell, double amountToPay, Date dueDate, Company company) {
 		long idToAdd;
 
@@ -49,6 +65,12 @@ public class ReminderController {
 		reminderList.add(reminderToAdd);
 	}
 	
+	/**
+	 * Pay a supplier an amount that you owe.
+	 * 
+	 * @param amount The amount that the user wishes to pay the supplier.
+	 * @param id The id of the reminder to be paid.
+	 */
 	public static void payAmountOutstanding(double amount, long id){
 		for(int i = reminderList.size()-1; i >= 0 ; i--){
 			if (reminderList.get(i).getId() == id){
@@ -63,6 +85,12 @@ public class ReminderController {
 		RemindersGUI.updateReminder();
 	}
 	
+	/**
+	 * Receive a payment from a customer.
+	 * 
+	 * @param amount The amount that the customer paid you.
+	 * @param id The id of the reminder to collect on.
+	 */
 	public static void collectAmountOwed(double amount, long id){
 		for(int i = reminderList.size()-1; i >= 0 ; i--){
 			if (reminderList.get(i).getId() == id){
